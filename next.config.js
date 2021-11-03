31cb09446ab711b2525371438c96576201e2f804
Steven <53283472+binolt@users.noreply.github.com>
2021-11-03 19:47:26 -0400

    update env variable configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    'CLOUDINARY_CLOUD_NAME': process.env.CLOUDINARY_CLOUD_NAME,
+    'CLOUDINARY_API_KEY': process.env.CLOUDINARY_API_KEY,
+    'CLOUDINARY_API_SECRET': process.env.CLOUDINARY_API_SECRET,
+  }
 }
